9dc4abaeb168b004ade635bdfd60ab0278760241
Sabrinsulthana <31950003+Sabrinsulthana@users.noreply.github.com>
2018-03-05 13:21:14 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+import org.neo.smartcontract.framework.SmartContract;
+import org.neo.smartcontract.framework.services.neo.Storage;
+
+public class HelloWorld extends SmartContract {
+
+    public static byte[] Main(String[] args){
+        Storage.put(Storage.currentContext(), "Greeting to the World", "Hello World!");
+        return Storage.get(Storage.currentContext(),"Greeting to the World");
+    }
+
+}
